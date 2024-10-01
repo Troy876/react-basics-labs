c4d6c60fd3cae5510687fa3e7a04e366e729fdc5
Troy <20098362@mail.wit.ie>
2024-10-01 14:33:22 +0100

    Tasky Lab 2 - Exercises done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -7,16 +7,17 @@ import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [taskState, setTaskState] = useState({
     tasks: [
-      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", description: "Clean up around the house", deadline: "Today", done: false }
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority: "Low" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority: "Medium" },
+      { id: 3, title: "Tidy up", description: "Clean up around the house", deadline: "Today", done: false, priority: "High" }
     ]
   });
 
   const [formState, setFormState] = useState({
     title: '',
     description: '',
-    deadline: ''
+    deadline: '',
+    priority: ''
   });
 
   const doneHandler = (taskIndex) => {
@@ -45,6 +46,9 @@ function App() {
       case "deadline":
         form.deadline = event.target.value;
         break;
+      case "priority":
+        form.priority = event.target.value;
+        break;
       default:
         form = formState;
     }
@@ -71,6 +75,7 @@ function App() {
           title={task.title}
           description={task.description}
           deadline={task.deadline}
+          priority={task.priority}
           key={task.id}
           done={task.done}
           markDone={() => doneHandler(index)}
@@ -78,7 +83,6 @@ function App() {
         />
       ))}
       <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
-      console.log(formState);
     </div>
   );
 }
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -3,10 +3,13 @@ import React from 'react';
 const Task = (props) => {
 
     return (
-        <div className="card" style={{ backgroundColor: props.done ? 'lightgrey' : '#5bb4c4' }}>
+        <div className="card" style={{ backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
             <p>{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className='description'>Description: {props.description}</p>
+            <div className='priorityOptions' style={{ backgroundColor: props.priority==="Low" ? 'green' : props.priority==="Medium" ? "blue" : "red" }}>
+            <p className='priority'>{props.priority}</p>
+            </div>
             <button onClick={props.markDone} className='doneButton'>Done</button>
             <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
